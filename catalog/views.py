f61cdcd550e07eacc8abdaf7b2b2358e60d3fe6e
Shengpo <bill130287@gmail.com>
2024-10-17 21:55:56 -0400

    feat: Add list and detail pages for books and authors
    - Map URL
    - Create view (class-based)
    - Create generic template
    - Paginate the data

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.http import Http404
 
 # Create your views here.
 
@@ -29,3 +31,37 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+    
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    # def book_detail_view(request, primary_key):
+    #     try:
+    #         book = Book.objects.get(pk=primary_key)
+    #     except Book.DoesNotExist:
+    #         raise Http404('Book does not exist')
+
+    #     return render(request, 'catalog/book_detail.html', context={'book': book})
+    
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    # def author_detail_view(request):
